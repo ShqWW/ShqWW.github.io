63e1440f0da5180706e164f6311d1bf05b6551ce
wsq <wsq@MyNAS>
2024-12-03 00:28:00 +0800

    Site updated: 2024-12-03 00:28:00

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1733156487885";
+const VERSION = "1733156876630";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",

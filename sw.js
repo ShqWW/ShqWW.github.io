a83b80d6981de3d14873fa34c2f4ba65145ebfa5
ShqWW <haorouw@outlook.com>
2026-04-29 20:00:41 +0800

    Site updated: 2026-04-29 20:00:40

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1777463650662";const preCache = ["/images/taichi.png","/images/banner3.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1777464039778";const preCache = ["/images/taichi.png","/images/banner3.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",

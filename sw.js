30471e067173ae71e7388d2107a46a1c2444f9e4
ShqWW <haorouw@outlook.com>
2026-04-29 14:37:22 +0800

    Site updated: 2026-04-29 14:37:22

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1777442831175";const preCache = ["/images/taichi.png","/images/banner3.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1777444641059";const preCache = ["/images/taichi.png","/images/banner3.png","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
